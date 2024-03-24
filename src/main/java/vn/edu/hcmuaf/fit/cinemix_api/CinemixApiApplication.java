1bb43e3c6a64cc16636f6fe72ca326882b9bc887
koi <huynhvahuuan3620@gmail.com>
2024-03-25 01:24:35 +0700

    initial

---
+++ src/main/java/vn/edu/hcmuaf/fit/cinemix_api/CinemixApiApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.cinemix_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemixApiApplication.class, args);
+    }
+
+}
