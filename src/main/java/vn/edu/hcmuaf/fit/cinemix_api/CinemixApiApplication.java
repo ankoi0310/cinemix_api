33fb8bb3323aef697606be75a083ff6da7b280ba
koi <huynhvahuuan3620@gmail.com>
2024-03-30 20:08:05 +0700

    chore: remove log4j2 config to use default back log

--- src/main/java/vn/edu/hcmuaf/fit/cinemix_api/CinemixApiApplication.java
+++ src/main/java/vn/edu/hcmuaf/fit/cinemix_api/CinemixApiApplication.java
@@ -1,11 +1,9 @@
 package vn.edu.hcmuaf.fit.cinemix_api;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@Slf4j
 @EnableAsync
 @SpringBootApplication
 public class CinemixApiApplication {
